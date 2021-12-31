770b7c12310199d029c8af7e097c58e614316682
Abayomi Aishida <yomicorperate@gmail.com>
2021-12-31 17:28:47 +0100

    Anuglar Components and Template Syntax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail-component';
+import { EventListComponent } from './events/events-list-component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventListComponent,
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: `<h1>Hello World</h1>
-  <img src="/assets/images/basic-shield.png"/>`,
+  template:  `
+    <nav-bar></nav-bar>
+    <events-list></events-list>
+  `,
   
 })
 export class EventsAppComponent {
